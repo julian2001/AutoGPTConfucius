f69fa732095eaa2a022b9bc29e19a4379b07bc58
julian2001 <julianr2000@yahoo.com>
2023-12-07 02:28:15 -0600

    Create package.swift
    Should allows the iOS app to talk to autogpt

---
+++ package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "AutoGPT",
+    platforms: [
+        .iOS(.v15) // Specify your iOS deployment target
+    ],
+    products: [
+        .library(
+            name: "AutoGPT",
+            targets: ["AutoGPT"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "AutoGPT",
+            dependencies: []),
+        .testTarget(
+            name: "AutoGPTTests",
+            dependencies: ["AutoGPT"]),
+    ]
+)
